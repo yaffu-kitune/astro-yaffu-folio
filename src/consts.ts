946cd7fc88829f4a0a225d98956ba1645ce8791b
yaffu-kitune <22cc0203@jec.ac.jp>
2023-07-14 17:19:59 +0900

    EditAllFile

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Yaffu Official Website';
+export const SITE_DESCRIPTION = 'Yaffu Official Website';
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,21 @@
-import { z, defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 
-const post = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    image: z.string().optional(),
-
-    canonical: z.string().url().optional(),
-
-    publishDate: z.date().or(z.string()).optional(),
-    draft: z.boolean().optional(),
-
-    excerpt: z.string().optional(),
-    category: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    author: z.string().optional(),
-  }),
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		heroImage: z.string().optional(),
+	}),
 });
 
-export const collections = {
-  post: post,
-};
+export const collections = { blog };
