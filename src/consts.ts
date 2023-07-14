ec88a961775472c6b9c622b7cd2c6d25479c0429
yaffu-kitune <22cc0203@jec.ac.jp>
2023-07-14 21:13:26 +0900

    edit blog and main page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Yaffu Official Website';
 export const SITE_DESCRIPTION = 'Yaffu Official Website';
